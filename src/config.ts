ae9c44805910ee6abcc3d7dbcf7e811a35c837b1
一杯为品 <154609713+INKEM@users.noreply.github.com>
2025-12-27 17:42:41 +0800

    new file:   public/images/avatar.webp 	modified:   src/config.ts 	deleted:    src/assets/images/avatar.webp 	deleted:    src/assets/images/avatar1.webp 	deleted:    src/assets/images/avatar2.webp

--- src/config.ts
+++ src/config.ts
@@ -336,7 +336,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "一杯为品",
 	bio: "不许平凡，不容悲欢。",
 	typewriter: {
