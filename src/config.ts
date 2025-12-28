7053378b4bcc737ac55aeeb801e6d37565382f54
一杯为品 <154609713+INKEM@users.noreply.github.com>
2025-12-28 23:29:22 +0800

    modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -423,7 +423,7 @@ export const commentConfig: CommentConfig = {
 export const announcementConfig: AnnouncementConfig = {
 	title: "公告", // 公告标题
 	content: "博客仍在建设中，部分功能尚未完善，敬请谅解~", // 公告内容
-	closable: true, // 允许用户关闭公告
+	closable: false, // 允许用户关闭公告
 	link: {
 		enable: true, // 启用链接
 		text: "了解更多", // 链接文本
