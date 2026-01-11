428bbaac06d3fe6829dd56ced98dfa9ddd8b8670
一杯为品 <154609713+INKEM@users.noreply.github.com>
2026-01-11 17:10:40 +0800

    modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -666,8 +666,8 @@ export const widgetConfigs = {
 } as const;
 
 export const umamiConfig = {
-	enabled: false, // 是否显示Umami统计
-	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
+	enabled: true, // 是否显示Umami统计
+	apiKey: import.meta.env.UMAMI_API_KEY || "api_HBQENsZPKiL6QB1MreTD3ShOkzctQEjA", // API密钥优先从环境变量读取，否则使用配置文件中的值
 	baseUrl: "https://api.umami.is", // Umami Cloud API地址
 	scripts: `
 <script defer src="https://cloud.umami.is/script.js" data-website-id="2336e06b-0952-4662-94be-7a28de5e7b4e"></script>  `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
